26612e4be885bcf6d739efb9c7d18bb42dd4c67e
vansh-valecha <valechavansh560@gmail.com>
2026-03-02 00:31:17 +0530

    Create natureofno.py

---
+++ 2- CONDITIONAL STATEMENTS/natureofno.py
@@ -0,0 +1,7 @@
+#to check no is positive,negative or zero
+num=int(input("enter no:"))
+if(num>0):
+    print(num,"is positive")
+elif(num<0):
+    print(num,"is negative")
+else:    print(num,"is zero")
