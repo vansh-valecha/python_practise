dbca622c937e793774aa6ab422cab7cae42784a2
vansh-valecha <valechavansh560@gmail.com>
2026-03-04 00:31:09 +0530

    Add income tax calculator for FY 2025-2026
    Implement income tax calculation based on income brackets.

---
+++ 2- CONDITIONAL STATEMENTS/incometaxcalculator.py
@@ -0,0 +1,18 @@
+#for fy 2025-2026
+income=int(input("enter your income:"))
+if(income<=400000):
+    print("no tax")
+elif(income<=800000):
+    tax=0.05*income
+    print("tax to be paid:",tax)
+elif(income<=1200000):
+    tax=0.1*income
+    print("tax to be paid:",tax)
+elif(income<=1600000):
+    tax=0.15*income
+    print("tax to be paid:",tax)
+elif(income<=2000000):
+    tax=0.2*income
+    print("tax to be paid:",tax)
+else:
+    tax=0.3*income
