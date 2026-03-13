9517db7e82318a97ee0f685147e7edde578fe6d9
vansh-valecha <valechavansh560@gmail.com>
2026-03-13 14:39:14 +0530

    Add student dictionary with marks and access examples

---
+++ 4- Dictionary & Set/excess_of_nested_dictionary.py
@@ -0,0 +1,17 @@
+student={
+    "NAME": "ABC",
+    "ROLL NO ": 1,
+    "MARKS": {
+        "PHYSICS": 78,
+        "CHEMISTRY": 85,
+        "MATHS": 92
+    }
+}
+print("DICTIONARY STYLE")
+print(student)
+print("\nACCESSING ONE BY ONE")
+print("NAME:", student["NAME"])
+print("ROLL NO:", student["ROLL NO "])
+print("MARKS IN CHEMISTRY:", student["MARKS"]["CHEMISTRY"])
+print("MARKS IN PHYSICS:", student["MARKS"]["PHYSICS"])
+print("MARKS IN MATHS:", student["MARKS"]["MATHS"])'''
