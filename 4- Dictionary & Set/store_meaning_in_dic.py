7b0ab1756748cf4d5cfd241ec249a82478ec7e81
vansh-valecha <valechavansh560@gmail.com>
2026-03-16 22:02:02 +0530

    Create store_meaning_in_dic.py

---
+++ 4- Dictionary & Set/store_meaning_in_dic.py
@@ -0,0 +1,8 @@
+dictionary={
+    "cat":"a small animal",
+    "table":["a piece of furniture,list of fact and figure"]
+}
+print(dictionary)
+#or second method
+print("meaning of cat:",dictionary["cat"])
+print("meaning of table:",dictionary["table"])
