f2f7f929ff18fdff0259f1c73e7c7af8b9335a4b
vansh-valecha <valechavansh560@gmail.com>
2026-03-17 01:15:17 +0530

    Create find_no_of_classes.py
    Implement a function to count unique subjects for classes.

---
+++ 4- Dictionary & Set/find_no_of_classes.py
@@ -0,0 +1,6 @@
+#Q.find the no of class required.
+stu_sub={
+    "python","java","c++","python","javascript","java","python","java","c++","c"   
+}
+print(stu_sub)
+print("no of class required:",len(stu_sub))
