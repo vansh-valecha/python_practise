6fcefe00f3a9cb3cbb9f6239f3ffb6baea0c0380
vansh-valecha <valechavansh560@gmail.com>
2026-03-26 02:49:23 +0530

    Create eve_odd_upto_100.py

---
+++ 5- loops/eve_odd_upto_100.py
@@ -0,0 +1,7 @@
+#print all the even & odd no form 1 to 100 using for loop
+print("even no are--")
+for i in range(2,102,2):
+    print(i)
+print("even no are--")    
+for i in range(1,101,2):
+    print(i)  
