93e4244dc6e6d49f39b9514388e49f0cd09ffde6
vansh-valecha <valechavansh560@gmail.com>
2026-04-01 00:58:10 +0530

    Create fact.py
    Implement factorial function and user input

---
+++ 6- functions & recursion /fact.py
@@ -0,0 +1,8 @@
+#factorial using function
+def fact(n):
+    result=1
+    for i in range(1,n+1):
+        result = result * i
+    return result
+n=int(input("enter number:"))
+print("factorial is ",fact(n))
